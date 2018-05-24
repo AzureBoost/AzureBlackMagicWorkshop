3f9838e0c0b1c210419bf09a087265b31e9d6b50
Tillmann Eitelberg <tillmann@eitelberg.de>
2018-05-24 11:58:51 +0200

    Add files via upload

---
+++ PowerShell/NewDisk.ps1
@@ -0,0 +1,5 @@
+﻿Get-Disk |
+Where partitionstyle -eq 'raw' |
+Initialize-Disk -PartitionStyle GPT -PassThru |
+New-Partition -AssignDriveLetter -UseMaximumSize |
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "DATA" -Force
